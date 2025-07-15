4f800dc80f0309a80b3a4b480783ca5adf0f80f5
Nila Akter <akternila1147@gmail.com>
2025-07-15 20:13:18 +0600

    second commit

--- src/Components/Roots/Root.jsx
+++ src/Components/Roots/Root.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
+import Homepage from '../HomePage/Homepage';
 
 const Root = () => {
     return (
         <>
         hello
+        <Homepage></Homepage>
         <Outlet></Outlet>
         </>
     );
